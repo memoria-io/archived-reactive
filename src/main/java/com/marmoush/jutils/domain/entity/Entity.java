47f43893663bd7460e0d3d28447767e3b3bb157b
Ismail Marmoush <marmoushismail@gmail.com>
2019-11-23 23:25:39 +0200

    entity
    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

---
+++ src/main/java/com/marmoush/jutils/domain/entity/Entity.java
@@ -0,0 +1,28 @@
+package com.marmoush.jutils.domain.entity;
+
+import java.util.Objects;
+
+public class Entity<T> {
+  public final String id;
+  public final T value;
+
+  public Entity(String id, T value) {
+    this.id = id;
+    this.value = value;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, value);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Entity<T> entity = (Entity<T>) o;
+    return Objects.equals(id, entity.id) && Objects.equals(value, entity.value);
+  }
+}
