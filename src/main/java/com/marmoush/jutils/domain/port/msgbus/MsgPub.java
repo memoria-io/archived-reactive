423a5072cc146598744d4108c0ec1ba1815b96c2
Ismail Marmoush <marmoushismail@gmail.com>
2020-01-14 22:45:22 +0200

    pubsub
    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

--- src/main/java/com/marmoush/jutils/domain/port/msgbus/MsgPub.java
+++ src/main/java/com/marmoush/jutils/domain/port/msgbus/MsgPub.java
@@ -3,8 +3,9 @@ package com.marmoush.jutils.domain.port.msgbus;
 import com.marmoush.jutils.domain.value.msg.Msg;
 import com.marmoush.jutils.domain.value.msg.PubResp;
 import io.vavr.control.Try;
+import org.apache.pulsar.client.api.PulsarClientException;
 import reactor.core.publisher.Flux;
 
 public interface MsgPub {
-  Flux<Try<PubResp>> pub(Flux<Msg> msgFlux, String topic, String partitionStr);
+  Flux<Try<PubResp>> pub(Flux<Msg> msgFlux, String topic, String partitionStr) throws PulsarClientException;
 }
