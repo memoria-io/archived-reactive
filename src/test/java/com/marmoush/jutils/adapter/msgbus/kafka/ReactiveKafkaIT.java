7e2fd645e9adfc6973ba78d6467d3fdd21c2b74a
Ismail Marmoush <marmoushismail@gmail.com>
2020-01-15 20:15:49 +0200

    kafka
    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

--- src/test/java/com/marmoush/jutils/adapter/msgbus/kafka/ReactiveKafkaIT.java
+++ src/test/java/com/marmoush/jutils/adapter/msgbus/kafka/ReactiveKafkaIT.java
@@ -1,16 +1,11 @@
 package com.marmoush.jutils.adapter.msgbus.kafka;
 
-import com.marmoush.jutils.domain.port.msgbus.MsgConsumer;
-import com.marmoush.jutils.domain.port.msgbus.MsgProducer;
-import com.marmoush.jutils.domain.value.msg.ConsumerResp;
 import com.marmoush.jutils.domain.value.msg.Msg;
-import com.marmoush.jutils.domain.value.msg.ProducerResp;
 import com.marmoush.jutils.utils.yaml.YamlUtils;
 import io.vavr.collection.Map;
 import io.vavr.control.Try;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
@@ -24,24 +19,35 @@ import java.util.Random;
 import static io.vavr.control.Option.some;
 
 public class ReactiveKafkaIT {
-  private final Map<String, Object> config = YamlUtils.parseYamlResource("kafka.yaml").get();
-  private final String PARTITION = "0";
-  private final int MSG_COUNT = 3;
+  private final Map<String, Object> config;
+
+  private final LinkedHashMap<String, Object> producerConf;
+  private final KafkaProducer<String, String> kafkaProducer;
+  private final KafkaMsgProducer msgProducer;
+
+  private final LinkedHashMap<String, Object> consumerConf;
+  private final KafkaConsumer<String, String> kafkaConsumer;
+  private final KafkaMsgConsumer msgConsumer;
+
+  public ReactiveKafkaIT() {
+    config = YamlUtils.parseYamlResource("kafka.yaml").get();
+    // Publisher
+    //noinspection unchecked
+    producerConf = (LinkedHashMap<String, Object>) config.get("producer").get();
+    kafkaProducer = new KafkaProducer<>(producerConf);
+    msgProducer = new KafkaMsgProducer(kafkaProducer, Schedulers.elastic(), Duration.ofSeconds(1));
+    //noinspection unchecked
+    consumerConf = (LinkedHashMap<String, Object>) config.get("consumer").get();
+    kafkaConsumer = new KafkaConsumer<>(consumerConf);
+    msgConsumer = new KafkaMsgConsumer(kafkaConsumer, Schedulers.elastic(), Duration.ofSeconds(1));
+  }
 
   @Test
   @DisplayName("Consumed messages should be same as published ones.")
   public void kafkaPubSub() {
     final String TOPIC = "topic-" + new Random().nextInt(1000);
-    @SuppressWarnings("unchecked")
-    var publisherConf = (LinkedHashMap<String, Object>) config.get("publisher").get();
-
-    var kafkaProducer = new KafkaProducer<String, String>(publisherConf);
-    var msgProducer = new KafkaMsgProducer(kafkaProducer, Schedulers.elastic(), Duration.ofSeconds(1));
-
-    @SuppressWarnings("unchecked")
-    var consumerConf = (LinkedHashMap<String, Object>) config.get("consumer").get();
-    var kafkaConsumer = new KafkaConsumer<String, String>(consumerConf);
-    var msgConsumer = new KafkaMsgConsumer(kafkaConsumer, Schedulers.elastic(), Duration.ofSeconds(1));
+    final String PARTITION = "0";
+    final int MSG_COUNT = 3;
 
     var msgs = Flux.interval(Duration.ofMillis(10)).map(i -> new Msg("Msg number" + i, some(i + ""))).take(MSG_COUNT);
     var publisher = msgProducer.produce(TOPIC, PARTITION, msgs);
