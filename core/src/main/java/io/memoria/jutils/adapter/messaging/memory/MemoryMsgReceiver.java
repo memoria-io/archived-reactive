3d1ce866ccc4b8f9039b46e986d94221a7f723a4
Ismail Marmoush <marmoushismail@gmail.com>
2020-07-26 01:00:24 +0200

    Correctly creating flux, with lazy evaluation (i.e using fromCallable) method instead of eager evaluation, this is a very sneaky bug because you might think an inmemory implementation is blocking safe since it works fast, but the problem here not the blocking safety but the eager calling of methods which is executed instantly; which means that before subscribing to the flux the result of eagerly executed function is saved inside the flux, and not called each time.

--- core/src/main/java/io/memoria/jutils/adapter/messaging/memory/MemoryMsgReceiver.java
+++ core/src/main/java/io/memoria/jutils/adapter/messaging/memory/MemoryMsgReceiver.java
@@ -3,17 +3,23 @@ package io.memoria.jutils.adapter.messaging.memory;
 import io.memoria.jutils.core.messaging.Message;
 import io.memoria.jutils.core.messaging.MessageFilter;
 import io.memoria.jutils.core.messaging.MsgReceiver;
+import io.vavr.control.Try;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
-public record MemoryMsgReceiver(Map<String, HashMap<Integer, LinkedList<Message>>>db, MessageFilter mf)
+public record MemoryMsgReceiver(Map<String, HashMap<Integer, Queue<Message>>>db, MessageFilter mf)
         implements MsgReceiver {
 
   @Override
   public Flux<Message> get() {
-    return Flux.fromIterable(db.get(mf.topic()).get(mf.partition())).skip(mf.offset());
+    return Mono.fromCallable(() -> {
+      var t = Try.of(() -> db.get(mf.topic()).get(mf.partition()));
+      return t.getOrElse(new LinkedList<>());
+    }).flatMapMany(Flux::fromIterable).skip(mf.offset());
   }
 }
--- core/src/main/java/io/memoria/jutils/adapter/messaging/memory/MemoryMsgSender.java
+++ core/src/main/java/io/memoria/jutils/adapter/messaging/memory/MemoryMsgSender.java
@@ -4,28 +4,23 @@ import io.memoria.jutils.core.messaging.Message;
 import io.memoria.jutils.core.messaging.MessageFilter;
 import io.memoria.jutils.core.messaging.MsgSender;
 import io.memoria.jutils.core.messaging.Response;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
-public record MemoryMsgSender(Map<String, HashMap<Integer, LinkedList<Message>>>db, MessageFilter mf)
-        implements MsgSender {
+public record MemoryMsgSender(Map<String, HashMap<Integer, Queue<Message>>>db, MessageFilter mf) implements MsgSender {
 
   @Override
-  public Flux<Response> apply(Flux<Message> msgFlux) {
-    return msgFlux.map(msg -> {
+  public Mono<Response> apply(Message msg) {
+    return Mono.fromCallable(() -> {
       db.putIfAbsent(mf.topic(), new HashMap<>());
       db.get(mf.topic()).putIfAbsent(mf.partition(), new LinkedList<>());
-      db.get(mf.topic()).get(mf.partition()).addLast(msg);
-      return db.get(mf.topic()).get(mf.partition()).size() - 1;
-    }).map(Response::new);
-  }
-
-  @Override
-  public Mono<Response> apply(Message message) {
-    return null;
+      db.get(mf.topic()).get(mf.partition()).add(msg);
+      var i = db.get(mf.topic()).get(mf.partition()).size() - 1;
+      return new Response(i);
+    });
   }
 }
