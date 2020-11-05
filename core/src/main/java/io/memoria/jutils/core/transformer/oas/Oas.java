904361b73f5090d9c2df2314d4cb0075cc971673
Ismail Marmoush <marmoushismail@gmail.com>
2020-11-05 16:56:02 +0200

    TBC

--- core/src/main/java/io/memoria/jutils/core/transformer/oas/Oas.java
+++ core/src/main/java/io/memoria/jutils/core/transformer/oas/Oas.java
@@ -1,40 +1,22 @@
 package io.memoria.jutils.core.transformer.oas;
 
-import io.memoria.jutils.core.transformer.oas.OasType.OasOneType;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public interface Oas {
-  OasType type();
-
   record OasEnum(String... values) {}
 
   record OasString(Optional<OasStringFormat> format,
                    Optional<Integer> minLength,
                    Optional<Integer> maxLength,
-                   Optional<OasEnum> isEnum) implements Oas {
-    @Override
-    public OasType type() {
-      return OasOneType.STRING;
-    }
-  }
+                   Optional<OasEnum> enumValues) implements Oas {}
 
-  record OasArray(OasType itemsType,
-                  Optional<Integer> minItems,
-                  Optional<Integer> maxItems,
-                  Optional<Boolean> uniqueItems) implements Oas {
-    @Override
-    public OasType type() {
-      return OasOneType.ARRAY;
-    }
-  }
+  record OasArray(Optional<Integer> minItems, Optional<Integer> maxItems, Optional<Boolean> uniqueItems)
+          implements Oas {}
 
-  record OasObjectDto(Map<String, Oas> properties, List<String> required) implements Oas {
-    @Override
-    public OasType type() {
-      return OasOneType.OBJECT;
-    }
-  }
+  record OasObject(Map<String, Oas> properties, List<String> required) implements Oas {}
+
+  record OasOneOf() implements Oas {}
+  record OasAllOf()implements Oas{}
 }
