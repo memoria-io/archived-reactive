3c6deab1289250b2c5766e2cef36932f80f13479
Ismail Marmoush <marmoushismail@gmail.com>
2020-11-12 14:10:03 +0200

    mapping jackson exceptions to property exception

---
+++ core/src/main/java/io/memoria/jutils/core/transformer/TransformerException.java
@@ -0,0 +1,24 @@
+package io.memoria.jutils.core.transformer;
+
+import io.vavr.collection.List;
+
+import java.io.IOException;
+
+public class TransformerException extends IOException {
+  public static TransformerException noMatchingTransformer(List<Class<?>> types) {
+    var classes = types.map(Class::getSimpleName).reduce((a, b) -> a + ", " + b);
+    return new TransformerException("No matching transformer found for any of [" + classes + "] ");
+  }
+
+  public static <T> TransformerException unsupportedDeserialization(Class<T> type) {
+    return new TransformerException("Deserialization is not supported for the type [%s]".formatted(type.getSimpleName()));
+  }
+
+  public static <T> TransformerException unsupportedSerialization(Class<T> type) {
+    return new TransformerException("Serialization is not supported for the type [%s]".formatted(type.getSimpleName()));
+  }
+
+  public TransformerException(String message) {
+    super(message);
+  }
+}
--- jackson-adapter/src/main/java/io/memoria/jutils/jackson/transformer/yaml/YamlJackson.java
+++ jackson-adapter/src/main/java/io/memoria/jutils/jackson/transformer/yaml/YamlJackson.java
@@ -1,14 +1,22 @@
 package io.memoria.jutils.jackson.transformer.yaml;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.memoria.jutils.core.transformer.PropertyException;
 import io.memoria.jutils.core.transformer.yaml.Yaml;
 import io.vavr.control.Try;
+
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.Predicates.instanceOf;
 
 public record YamlJackson(ObjectMapper mapper) implements Yaml {
 
   @Override
   public <T> Try<T> deserialize(String str, Class<T> tClass) {
-    return Try.of(() -> mapper.readValue(str, tClass));
+    //noinspection unchecked
+    return Try.of(() -> mapper.readValue(str, tClass))
+              .mapFailure(Case($(instanceOf(JacksonException.class)), e -> new PropertyException(e.getMessage())));
   }
 
   @Override
