291562141260d6ea1a8a656bb0ef6e112a960cb7
Ismail Marmoush <marmoushismail@gmail.com>
2020-11-27 21:31:43 +0200

    introducing event meta object

--- jackson-adapter/src/test/java/io/memoria/jutils/jackson/transformer/NameCreated.java
+++ jackson-adapter/src/test/java/io/memoria/jutils/jackson/transformer/NameCreated.java
@@ -4,4 +4,4 @@ import io.memoria.jutils.core.eventsourcing.event.Event;
 import io.memoria.jutils.core.eventsourcing.event.Meta;
 import io.memoria.jutils.core.value.Id;
 
-public record NameCreated(Id id, String name,Meta meta) implements Event {}
+public record NameCreated(Id id, String name, Meta meta) implements Event {}
