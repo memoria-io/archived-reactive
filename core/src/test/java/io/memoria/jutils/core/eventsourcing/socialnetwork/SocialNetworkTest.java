2b63e0d2e6ab7d09985fb2ea429ed227f826422a
Ismail Marmoush <marmoushismail@gmail.com>
2020-12-05 17:48:38 +0200

    applytwice typo

--- core/src/test/java/io/memoria/jutils/core/eventsourcing/socialnetwork/SocialNetworkTest.java
+++ core/src/test/java/io/memoria/jutils/core/eventsourcing/socialnetwork/SocialNetworkTest.java
@@ -7,13 +7,13 @@ class SocialNetworkTest {
   void sqlHandler() {
     SocialNetworkSuite.applyAllCommands(new SocialNetworkTestData(true));
     SocialNetworkSuite.applyOneCommand(new SocialNetworkTestData(true));
-    SocialNetworkSuite.applyOneCommand(new SocialNetworkTestData(true));
+    SocialNetworkSuite.applyTwice(new SocialNetworkTestData(true));
   }
 
   @Test
   void statefulHandler() {
     SocialNetworkSuite.applyAllCommands(new SocialNetworkTestData(false));
     SocialNetworkSuite.applyOneCommand(new SocialNetworkTestData(false));
-    SocialNetworkSuite.applyOneCommand(new SocialNetworkTestData(false));
+    SocialNetworkSuite.applyTwice(new SocialNetworkTestData(false));
   }
 }
