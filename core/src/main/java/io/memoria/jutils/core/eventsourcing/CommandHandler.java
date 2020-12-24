98a9ce2d6f6f028929ef2b54904488177b123a56
Ismail Marmoush <marmoushismail@gmail.com>
2020-12-24 17:29:04 +0200

    adding back multiple cmds

--- core/src/main/java/io/memoria/jutils/core/eventsourcing/CommandHandler.java
+++ core/src/main/java/io/memoria/jutils/core/eventsourcing/CommandHandler.java
@@ -4,4 +4,8 @@ import io.vavr.Function1;
 import reactor.core.publisher.Flux;
 
 @FunctionalInterface
-public interface CommandHandler<C extends Command> extends Function1<C, Flux<Event>> {}
+public interface CommandHandler<C extends Command> extends Function1<C, Flux<Event>> {
+  default Flux<Event> apply(Flux<C> cmds) {
+    return cmds.concatMap(this);
+  }
+}
