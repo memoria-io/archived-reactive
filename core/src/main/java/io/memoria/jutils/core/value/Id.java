7f3cd89b1b2f2c12b4591f41e805c94181402d50
Ismail Marmoush <marmoushismail@gmail.com>
2020-12-24 02:20:39 +0200

    adding Id of long

--- core/src/main/java/io/memoria/jutils/core/value/Id.java
+++ core/src/main/java/io/memoria/jutils/core/value/Id.java
@@ -1,6 +1,10 @@
 package io.memoria.jutils.core.value;
 
 public record Id(String value) {
+  public static Id of(long id) {
+    return new Id(id + "");
+  }
+
   public Id {
     if (value == null || value.isEmpty())
       throw new IllegalArgumentException("Id value is null or empty.");
