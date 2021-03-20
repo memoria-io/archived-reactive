0d3b691acbb9b88e838f9fec32df13d005c462d1
Ismail Marmoush <marmoushismail@gmail.com>
2021-03-20 20:15:22 +0200

    remove pulsar rename modules

--- jr2/src/main/java/io/memoria/jutils/jes/r2/R2CommandHandler.java
+++ jr2/src/main/java/io/memoria/jutils/jes/r2/R2CommandHandler.java
@@ -14,6 +14,7 @@ import static io.memoria.jutils.jes.r2.SqlUtils.safeTableName;
 /**
  * An SQL based commandHandler
  */
+@SuppressWarnings("ClassCanBeRecord")
 public final class R2CommandHandler<S, C extends Command> implements CommandHandler<C> {
   private final ConnectionFactory connectionFactory;
   private final TextTransformer textTransformer;
