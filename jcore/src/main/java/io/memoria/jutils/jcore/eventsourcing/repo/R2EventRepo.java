fde6eb70ffd128821876ec9777b3ee215004a24d
Ismail Marmoush <marmoushismail@gmail.com>
2021-06-24 19:31:21 +0200

    clean up

--- jcore/src/main/java/io/memoria/jutils/jcore/eventsourcing/repo/R2EventRepo.java
+++ jcore/src/main/java/io/memoria/jutils/jcore/eventsourcing/repo/R2EventRepo.java
@@ -7,9 +7,8 @@ import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.IsolationLevel;
 import io.r2dbc.spi.Result;
-import io.r2dbc.spi.Row;
-import io.r2dbc.spi.Statement;
 import io.vavr.collection.List;
+import io.vavr.control.Try;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -33,9 +32,7 @@ public class R2EventRepo implements EventRepo {
   public Mono<Void> createTopic(String topic) {
     return Mono.from(connectionFactory.create()).flatMap(con -> {
       // Configure Transaction
-      con.setAutoCommit(false);
-      con.beginTransaction();
-      con.setTransactionIsolationLevel(IsolationLevel.READ_COMMITTED);
+      con.setAutoCommit(true);
       var tableName = safeTableName(topic);
       var sql = """
                 CREATE TABLE IF NOT EXISTS %s (
@@ -45,7 +42,7 @@ public class R2EventRepo implements EventRepo {
                 PRIMARY KEY (id)
               )
               """.formatted(tableName);
-      return SqlUtils.exec(con.createStatement(sql)).last().doOnSuccess(i -> con.commitTransaction());
+      return SqlUtils.exec(con.createStatement(sql)).last();
     }).then();
   }
 
@@ -56,39 +53,37 @@ public class R2EventRepo implements EventRepo {
       con.setAutoCommit(false);
       con.beginTransaction();
       con.setTransactionIsolationLevel(IsolationLevel.READ_COMMITTED);
-      // Append events
-      var st = Mono.from(insert(con, topic, events).execute()).map(Result::getRowsUpdated).flatMap(Mono::from);
-      return st.doOnSuccess(s -> con.commitTransaction()).doOnError(s -> con.rollbackTransaction());
+      // Insert events
+      return insert(con, topic, events).doOnSuccess(s -> con.commitTransaction())
+                                       .doOnError(s -> con.rollbackTransaction());
     });
   }
 
   @Override
   public Mono<List<Event>> find(String topic) {
-    return Mono.from(connectionFactory.create()).flatMap(con -> {
-      var tableName = safeTableName(topic);
-      return query(con, topic).collectList();
-    }).map(List::ofAll);
+    return Mono.from(connectionFactory.create()).flatMap(con -> query(con, topic));
   }
 
-  private Flux<Event> query(Connection connection, String tableName) {
+  private Mono<List<Event>> query(Connection connection, String tableName) {
     var sql = "SELECT %s FROM %s ORDER BY id".formatted(PAYLOAD_COL, tableName);
     var execute = connection.createStatement(sql).execute();
-    return Flux.<Result>from(execute).flatMap(r -> r.map((row, rowMetadata) -> row)).map(this::rowToEvent);
+    return Flux.<Result>from(execute)
+               .flatMap(r -> r.map((row, rowMetadata) -> row))
+               .map(row -> row.get(PAYLOAD_COL, String.class))
+               .map(row -> textTransformer.deserialize(row, Event.class))
+               .map(Try::get)
+               .collectList()
+               .map(List::ofAll);
   }
 
-  private Event rowToEvent(Row row) {
-    var eventString = row.get(PAYLOAD_COL, String.class);
-    return this.textTransformer.deserialize(eventString, Event.class).get();
-  }
-
-  private Statement insert(Connection connection, String tableName, List<Event> events) {
+  private Mono<Integer> insert(Connection connection, String tableName, List<Event> events) {
     var sql = "INSERT INTO %s (%s, %s) ".formatted(tableName, CREATED_AT_COL, PAYLOAD_COL) + "VALUES($1, $2)";
     var st = connection.createStatement(sql);
     for (Event e : events) {
-      var eventPayload = this.textTransformer.serialize(e).get();
+      var eventPayload = textTransformer.serialize(e).get();
       st.bind("$1", Timestamp.valueOf(e.createdAt()).toString()).bind("$2", eventPayload);
       st.add();
     }
-    return st;
+    return Mono.from(st.execute()).map(Result::getRowsUpdated).flatMap(Mono::from);
   }
 }
